fe56c71b9d2286038ad1d8909d0f98a74d38ac56
Guilherme Malosti Fonseca <gmathos77@gmail.com>
2023-03-16 12:57:42 -0300

    Criando back da api e adicionando gitignore

---
+++ PortifolioMalostiApi/Data/PorifolioContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortifolioMalostiApi.Model;
+
+namespace PortifolioMalostiApi.Data
+{
+    public class PorifolioContext : DbContext
+    {
+        public PorifolioContext(DbContextOptions<PorifolioContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
---
+++ PortifolioMalostiApi/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using PortifolioMalostiApi.Data;
+using PortifolioMalostiApi.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlServer<PorifolioContext>(builder.Configuration.GetConnectionString("ServerConnection"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseCors(p => p
+    .AllowAnyHeader()
+    .AllowAnyOrigin()
+    .AllowAnyMethod());
+
+app.MapPost("/contacts", async (PorifolioContext context, Contact contact) => 
+{
+    await context.AddAsync(contact);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(contact);
+})
+.WithOpenApi();
+
+app.MapGet("/contacts", async (PorifolioContext context) =>
+{
+    var contacts = await context.Contacts.AsNoTracking().ToListAsync();
+    return Results.Ok(contacts);
+});
+
+app.Run();
+
+
